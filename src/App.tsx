b15d3e94792d5dcbfa91da3adbf04e3dca5e07b0
Le Xuan Dai <Dyan@nexondv.com>
2023-07-21 17:28:44 +0700

    add apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
+
+type Pokemon = {
+  id: string;
+  name: string;
+  weight: number;
+  height: number;
+  sprites: {
+    front_default: string;
+    front_shiny: string;
+    back_default: string;
+    back_shiny: string;
+  };
+};
+
+const GET_POKEMON = gql`
+  query GetPokemon($name: String!) {
+    getPokemon(name: $name) {
+      id
+      name
+      weight
+      height
+      sprites {
+        front_default
+        front_shiny
+        back_default
+        back_shiny
+      }
+    }
+  }
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const { loading, error, data } = useQuery(GET_POKEMON, {
+    variables: { name: "pikachu" },
+  });
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error : {error.message}</p>;
   return (
     <>
+      <h1>{data?.getPokemon?.name}</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +68,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
